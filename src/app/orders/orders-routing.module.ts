d38138add6dbbb6bc29ef6ce9e5f15f4a00f4959
christophe gueroult <christophegueroult@gmail.com>
2020-05-12 15:50:23 +0100

    install api rest json server

--- src/app/orders/orders-routing.module.ts
+++ src/app/orders/orders-routing.module.ts
@@ -4,7 +4,7 @@ import { PageListOrdersComponent } from './pages/page-list-orders/page-list-orde
 
 
 const routes: Routes = [
-  { path: '', component: PageListOrdersComponent },
+  { path: 'orders', component: PageListOrdersComponent },
 ];
 
 @NgModule({
