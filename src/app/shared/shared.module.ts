abf849a52a3168d79efd233e382602d3b74aac32
christophe gueroult <christophegueroult@gmail.com>
2020-05-13 11:21:23 +0100

    @Input() table dynamique

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TemplatesModule } from '../templates/templates.module';
+import { TableauLightComponent } from './components/tableau-light/tableau-light.component';
+import { ButtonComponent } from './components/button/button.component';
 
 
 
 @NgModule({
-  declarations: [],
-  exports: [TemplatesModule],
+  declarations: [TableauLightComponent, ButtonComponent],
+  exports: [TemplatesModule, TableauLightComponent, ButtonComponent],
   imports: [
     CommonModule,
     TemplatesModule
